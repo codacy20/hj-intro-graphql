d9b40420a925dd7ca256a93e33e55ca983a5bd4f
codacy20 <kmuc72@gmail.com>
2019-05-23 09:34:10 +0200

    code jam stuff

--- src/index.js
+++ src/index.js
@@ -2,18 +2,55 @@ const { ApolloServer, gql, PubSub } = require("apollo-server");
 const movies = require("./mocks/movies");
 const categories = require("./mocks/categories");
 const { getMoviesForCategory } = require("./utils");
+var _ = require("lodash");
 
 const pubsub = new PubSub();
 
 const typeDefs = gql`
   type Query {
     hello: String!
+    getMovie(id: Int!): Movie!
+    getMovies(name: String): [Movie!]
+    getCategories: [Category!]
+    addVote(movie_id: Int): Movie!
+  }
+  type Movie {
+    vote_count: Int
+    id: Int
+    video: Boolean
+    vote_average: Float
+    title: String
+    popularity: Float
+    poster_path: String
+    original_language: String
+    original_title: String
+    category_ids: [Int]
+    category: Category
+    backdrop_path: String
+    adult: Boolean
+    overview: String
+    release_date: String
+  }
+  type Category {
+    id: Int
+    name: String
+  }
+  type Mutation {
+    addVote(movie_id: ID!): Movie!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: (parent, args, ctx, info) => "Hello world!"
+    hello: (parent, args, ctx, info) => "Hello world!!",
+    getMovie: (parent, { id }, ctx, info) => _.find(movies, { id: id }),
+    getMovies: (parent, { name }, ctx, info) => getMoviesForCategory(name),
+    getCategories: (parent, args, ctx, info) => categories,
+    addVote: (parent, { movie_id }, ctx, info) => {
+      let movie = _.find(movies, { id: movie_id });
+      movie.vote_count++;
+      return movie;
+    }
   }
 };
 
